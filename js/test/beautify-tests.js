de9e5b46faa119d541e6adfcb73401792190fa9e
Martin Hansen <martin@martinhansen.no>
2013-11-12 12:27:56 +0100

    Fixed typo in test for @import gh-358

--- js/test/beautify-tests.js
+++ js/test/beautify-tests.js
@@ -1735,7 +1735,7 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
         btc("#bla, #foo{color:red}", "#bla,\n#foo {\n\tcolor: red\n}\n");
         btc("@media print {.tab{}}", "@media print {\n\t.tab {}\n}\n");
         btc("@import 'custom.css';.rule{}", "@import 'custom.css';\n.rule {}\n");
-        btc("@import url('bluish.css') projection,tv;.rule{}", "@import url('bluish.css') projection, tv;;\n.rule {}\n");
+        btc("@import url('bluish.css') projection,tv;.rule{}", "@import url('bluish.css') projection, tv;\n.rule {}\n");
 
         // comments
         btc("/* test */", "/* test */\n");
